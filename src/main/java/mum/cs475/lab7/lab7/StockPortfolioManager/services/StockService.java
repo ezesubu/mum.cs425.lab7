fcd6edc3081b698c85766778f2ea8475dcf294e4
Ezequiel Suarez <ezesubu@gmail.com>
2018-08-08 18:56:18 -0500

    Lab 7 StockPorfolio

---
+++ src/main/java/mum/cs475/lab7/lab7/StockPortfolioManager/services/StockService.java
@@ -0,0 +1,7 @@
+package mum.cs475.lab7.lab7.StockPortfolioManager.services;
+
+import mum.cs475.lab7.lab7.StockPortfolioManager.models.Stock;
+
+public interface StockService {
+    double getPrice(Stock stock);
+}
---
+++ src/test/java/mum/cs475/lab7/lab7/StockPortfolioManager/models/StockTest.java
@@ -0,0 +1,12 @@
+package mum.cs475.lab7.lab7.StockPortfolioManager.models;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class StockTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
